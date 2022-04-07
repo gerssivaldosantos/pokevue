ffb6bd69b369583ae42bd7db9e4d2054ebe2b231
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-04-06 23:17:10 -0300

    feat: create navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <nav class="navbar">
+        <div class="logo-wrapper">
+            <img
+                class="logo"
+                src="https://freepikpsd.com/file/2019/10/pokemon-icons-png-6-Transparent-Images.png"
+                alt="pokemon-logo"
+            />
+        </div>
+        <div class="title-wrapper">
+            <h1 class="title">PokeVue</h1>
+        </div>
+    </nav>
+</template>
+
+<style>
+.navbar {
+    background-color: rgb(0, 141, 82);
+    height: 70px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+}
+
+.logo {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+}
+
+.title{
+    color: rgb(30, 30, 30);
+    font-size: 40px;
+    font-weight: bold;
+    margin-left: 10px;
+}
+</style>
