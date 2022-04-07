a42797ff98b5b019321ffd83196cec60cc00abbf
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-04-06 23:17:00 -0300

    feat: add navbar and pokelist components

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,27 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue';
+import PokeList from './components/PokeList.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" msg2="Olá" />
+  <Navbar />
+  <PokeList/>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: rgb(30, 30, 30);
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+* {
+  font-family: sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  text-decoration: none;
 }
 </style>
